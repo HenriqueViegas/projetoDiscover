290c7d560d389f108163a72cd9162d731fcbf29a
Henrique Viegas <daggostiniviegas@gmail.com>
2023-07-05 08:51:18 -0300

    alteração imagem e links. Finalização projeto

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.style.border = "2px solid #6b6868"
     img.setAttribute("alt", "foto do usuário no light mode")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar.jpg")
+    img.style.border = "2px solid #fff"
     img.setAttribute("alt", "foto do usuário no dark mode")
   }
 }
